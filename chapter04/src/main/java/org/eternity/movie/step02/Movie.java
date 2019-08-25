1f1a80aca4bbebbae90daec9cb015bba233758e9
jungcollin <1019jsw@daum.net>
2019-08-26 08:16:38 +0900

    Update Movie.java

--- chapter04/src/main/java/org/eternity/movie/step02/Movie.java
+++ chapter04/src/main/java/org/eternity/movie/step02/Movie.java
@@ -19,18 +19,6 @@ public class Movie {
     private Money discountAmount;
     private double discountPercent;
 
-    public Movie(String title, Duration runningTime, Money fee, double discountPercent, DiscountCondition... discountConditions) {
-        this(MovieType.PERCENT_DISCOUNT, title, runningTime, fee, Money.ZERO, discountPercent, discountConditions);
-    }
-
-    public Movie(String title, Duration runningTime, Money fee, Money discountAmount, DiscountCondition... discountConditions) {
-        this(MovieType.AMOUNT_DISCOUNT, title, runningTime, fee, discountAmount, 0, discountConditions);
-    }
-
-    public Movie(String title, Duration runningTime, Money fee) {
-        this(MovieType.NONE_DISCOUNT, title, runningTime, fee, Money.ZERO, 0);
-    }
-
     private Movie(MovieType movieType, String title, Duration runningTime, Money fee, Money discountAmount, double discountPercent,
                   DiscountCondition... discountConditions) {
         this.movieType = movieType;
